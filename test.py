1089483098d835806a3b68a7ed19575defe4d4d1
Tzedakis Charidimos <harris.jedakis@gmail.com>
2018-06-26 00:57:11 +0300

    initial commit

---
+++ test.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# OpenCV ==> (B,G,R)
+# plt ==> (R,G,B)
+
+#Read Image
+#img = cv2.imread('baboon.png')
+img = cv2.imread('baboon.png', cv2.IMREAD_GRAYSCALE)
+# IMREAD_COLOR = 1 
+# IMREAD_UNCHANGED = -1 
+
+#Display Image
+#cv2.imshow('image',img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+
+plt.imshow(img, cmap='gray', interpolation='bicubic')
+plt.plot([50,100],[80,100], 'c', linewidth=5)
+plt.show()
+
+
+#Applying Grayscale filter to image
+#gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#cv2.imshow('image',gray)
+
+
+
+#Saving filtered image to new file
+#cv2.imwrite('graytest.jpg',gray)
